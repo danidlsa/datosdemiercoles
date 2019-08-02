8f104336d35cbdf3cfa125eb78345b3fbf009015
Daniela de los Santos <43679304+danidlsa@users.noreply.github.com>
2019-08-02 11:11:25 -0300

    Update la casa de papel.R

--- la casa de papel.R
+++ la casa de papel.R
@@ -1,7 +1,7 @@
 #Nota: el preprocesamiento, salvo algunas modificaciones propias, es forkeado del repo de /picanum
 #Ver: https://github.com/picanum/DatosDeMiercoles 
 
-setwd("C:/Users/Usuario/Dropbox/datos de miércoles")
+setwd("C:/Users/Usuario/Dropbox/datos de miÃ©rcoles")
 rm(list=ls())
 dat <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-07-31/la_casa_de_papel.csv")
 
@@ -24,41 +24,41 @@ windowsFonts(sans="Segoe UI")
 loadfonts(device="win")
 loadfonts(device="postscript")
 
-#Añadimos una variable que indique qué episodio es de todos (no sólo dentro de una temporada). Lo hacemos "a mano" mediante ifelse
+#AÃ±adimos una variable que indique quÃ© episodio es de todos (no sÃ³lo dentro de una temporada). Lo hacemos "a mano" mediante ifelse
 
 dat$epi_temp <- dat$episodio + ifelse(dat$temporada == 1, 0, ifelse(dat$temporada == 2, 9, 15))
 
-####CREACIÓN DE LA MATRIZ DE TÉRMINOS####
+####CREACIÃ“N DE LA MATRIZ DE TÃ‰RMINOS####
 
 
-#Con esta función quitamos los paréntesis de los subtítulos ya que no forman parte del diálogo en sí
+#Con esta funciÃ³n quitamos los parÃ©ntesis de los subtÃ­tulos ya que no forman parte del diÃ¡logo en sÃ­
 quitarparentesis <- function(x) gsub("\\s*\\([^\\)]+\\)","",as.character(x))
 
-#Con esta función aplicamos un preprocesamiento basado en quitar palabras stoppers, signos de puntuación, números y ponerlo todo en minúsculas
+#Con esta funciÃ³n aplicamos un preprocesamiento basado en quitar palabras stoppers, signos de puntuaciÃ³n, nÃºmeros y ponerlo todo en minÃºsculas
 preprocesamiento <- function(x) removeWords(removePunctuation(removeNumbers(tolower(x))),stopwords("spanish"))
 
 dat$texto2 <- preprocesamiento(quitarparentesis(dat$texto))
 
-#La función de preprocesamiento no quita los símbolos de apertura de exclamación ni de interrogación, así que los quitamos también
-dat$texto2 <- str_remove_all(dat$texto2, "¡")
-dat$texto2 <- str_remove_all(dat$texto2, "¿")
+#La funciÃ³n de preprocesamiento no quita los sÃ­mbolos de apertura de exclamaciÃ³n ni de interrogaciÃ³n, asÃ­ que los quitamos tambiÃ©n
+dat$texto2 <- str_remove_all(dat$texto2, "Â¡")
+dat$texto2 <- str_remove_all(dat$texto2, "Â¿")
 
 
 dat$texto2 <- iconv(dat$texto2,from="UTF-8",to="ASCII//TRANSLIT")
 
-#Creamos la matriz de términos y rescatamos aquellos que se hayan mencionado más de 50 veces a lo largo de la serie
+#Creamos la matriz de tÃ©rminos y rescatamos aquellos que se hayan mencionado mÃ¡s de 50 veces a lo largo de la serie
 DTM1 <- DocumentTermMatrix(Corpus(VectorSource(t(dat$texto2))))
 DTM1<-as.matrix(DTM1)
 DTM1<-as.data.frame(DTM1)
 DTM1<-DTM1[,which(apply(DTM1,2,sum) > 10)]
 
-#Unimos la matriz de términos al data.frame original... y dejamos respirar un poco a la memoria RAM :')
+#Unimos la matriz de tÃ©rminos al data.frame original... y dejamos respirar un poco a la memoria RAM :')
 dat <- data.frame(dat,DTM1)
 rm("DTM1")
 gc()
 
 
-#En dat2 almacenaremos el número de veces que se repite un término en cada episodio
+#En dat2 almacenaremos el nÃºmero de veces que se repite un tÃ©rmino en cada episodio
 dat2 <- reshape2::melt(dat[,c(7,9:700)], id.vars = "epi_temp")
 
 dat2$variable <- as.character(dat2$variable)
@@ -66,7 +66,7 @@ dat2$variable <- as.character(dat2$variable)
 
 dat2 <- aggregate(value ~ epi_temp + variable, dat2, FUN=sum)
 
-#Acto seguido sacamos la duración de cada episodio, y la unimos a dat2
+#Acto seguido sacamos la duraciÃ³n de cada episodio, y la unimos a dat2
 tiempos <- aggregate(tiempo_salida ~ epi_temp, dat, FUN = function(x) max(x)/60)
 dat2 <- merge(dat2, tiempos, by = "epi_temp")
 
@@ -104,16 +104,22 @@ dat3[4,2]<- 380
 
 head(dat3)
 
-x <- ggplot(dat3,
+peg <- readPNG("pegatina 2.png")
+g <- rasterGrob(peg, interpolate=TRUE)
+
+
+x<- ggplot(dat3,
   aes(label = variable, angle=angle, color=value, size=value)) +
   geom_text_wordcloud_area(
-    mask = png::readPNG("casa.png"),
+    mask = png::readPNG("pegatina-dali-la-casa-de-papel_ok.png"),
   rm_outside = TRUE)  +
   theme_minimal() +
   scale_color_gradient(low="darkred", high="red") +
-  labs (title="La casa de papel: palabras más repetidas",
+  labs (title="La casa de papel: palabras mÃ¡s repetidas",
 	subtitle="Temporadas 1-3",
-	caption="@danidlsa")
+	caption="@danidlsa") +
+  scale_size_area(max_size = 6) +
+ annotation_custom(g, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
 
 pdf("la casa de papel.pdf", height=6, width=6)
 x
